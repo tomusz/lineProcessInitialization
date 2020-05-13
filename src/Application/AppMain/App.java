421f5b6172f2da6306f185dbc1f8d0746fe60d93
Tomasz Wachowicz <wachowicztomek@gmail.com>
2020-05-13 19:26:14 +0200

    logic for first worker moved to the moment of ArrayList initialization, i and j replaced with correct variables names

--- src/Application/AppMain/App.java
+++ src/Application/AppMain/App.java
@@ -1,35 +1,33 @@
 package Application.AppMain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        int amountOfPLC = 1100;
-        int amountOfWorkers = 1100;
-        List<String> listOfProgrammableLogicControllerStatuses = new ArrayList<>();
-        for (int i=1;i<=amountOfWorkers;i++) {
-            for (int j=1;j<=amountOfPLC;j++) {
-                if (j%i==0) {
-                    if (i == 1) {
-                        listOfProgrammableLogicControllerStatuses.add("RUN");
-                    } else {
-                        listOfProgrammableLogicControllerStatuses.set(j-1,
-                                togglePLCStatus(listOfProgrammableLogicControllerStatuses.get(j-1)));
-                    }
+        List<String> listOfProgrammableLogicControllerStatuses = new ArrayList<>(
+                Collections.nCopies(1100, "RUN"));
+        for (int workerNumber = 2; workerNumber <= 1100; workerNumber++) {
+            for (int pLCNumber = 1; pLCNumber <= listOfProgrammableLogicControllerStatuses.size(); pLCNumber++) {
+                if (pLCNumber % workerNumber == 0) {
+                    listOfProgrammableLogicControllerStatuses.set(pLCNumber - 1,
+                            togglePLCStatus(listOfProgrammableLogicControllerStatuses.get(pLCNumber - 1)));
                 }
             }
         }
         printAmountOfPLCInStatusRun(listOfProgrammableLogicControllerStatuses);
     }
+
     private static String togglePLCStatus(String currentStatus) {
-        if (currentStatus.equals("RUN")){
+        if (currentStatus.equals("RUN")) {
             return "PROGRAM";
         } else {
             return "RUN";
         }
     }
+
     private static void printAmountOfPLCInStatusRun(List<String> PLCList) {
         int counter = 0;
         for (String plc : PLCList) {
@@ -39,5 +37,4 @@ public class App {
         }
         System.out.println("Amount of Programmable Logic Controller in status RUN is " + counter);
     }
-
 }
